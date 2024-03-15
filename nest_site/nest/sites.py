cb23d587cf65d80650623dd5ba453a521503fe8d
Zhi Li <zli@netflix.com>
2024-03-14 21:16:25 -0700

    Update demo_live_a/b instruction with Esc key.

--- nest_site/nest/sites.py
+++ nest_site/nest/sites.py
@@ -1020,9 +1020,9 @@ class NestSite(ExperimentMixin):
     @method_decorator(never_cache)
     def demo_live_a(self, request, extra_context=None):
         page = Samviq5dPage({
-            'title': 'Demo: how video bitrate (Mbps) affects quality - Part A',
+            'title': 'Demo: how bitrate (Mbps) affects video quality - Clip A',
             'instruction_html':
-                """<p> Instruction: Click on each "x Mbps" button to begin playing a video. Video will loop. <em> Press "space" key or double click the mouse </em> to exit.</p>""",  # noqa E501
+                """<p> Instruction: Click on each "x Mbps" button to begin playing a video. Video will loop. <em> Press "Esc" key or double click the mouse </em> to exit.</p>""",  # noqa E501
             'question':
                 """Using the 8 Mbps video as a reference, how would you rate the quality of each video? "Shameful" means you will be in shame if the video is played on Netflix service; "Acceptable" means you will accept the video to be played on Netflix; "Flawless" means you don't see a visual difference from the 8 Mbps reference.""",  # noqa E501
             'choices':
@@ -1054,9 +1054,9 @@ class NestSite(ExperimentMixin):
     @method_decorator(never_cache)
     def demo_live_b(self, request, extra_context=None):
         page = Samviq5dPage({
-            'title': 'Demo: how video bitrate (Mbps) affects quality - Part B',
+            'title': 'Demo: how bitrate (Mbps) affects video quality - Clip B',
             'instruction_html':
-                """<p> Instruction: Click on each "x Mbps" button to begin playing a video. Video will loop. <em> Press "space" key or double click the mouse </em> to exit.</p>""",  # noqa E501
+                """<p> Instruction: Click on each "x Mbps" button to begin playing a video. Video will loop. <em> Press "Esc" key or double click the mouse </em> to exit.</p>""",  # noqa E501
             'question':
                 """Using the 8 Mbps video as a reference, how would you rate the quality of each video? "Shameful" means you will be in shame if the video is played on Netflix service; "Acceptable" means you will accept the video to be played on Netflix; "Flawless" means you don't see a visual difference from the 8 Mbps reference.""",  # noqa E501
             'choices':
